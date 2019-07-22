63630f6795d9ba62014e756b96be8d8ebc0f7477
aislan rafael <aislanmaster@gmail.com>
2019-07-22 08:29:26 -0300

    implementando um método em ExtratorArgumentoUrl para retornar as moedas

--- extrator_argumento_url.py
+++ extrator_argumento_url.py
@@ -1,13 +1,32 @@
 class ExtratorArgumentoUrl:
     def __init__(self,url):
-        if url.url_eh_valida(url):
+        if self.url_eh_valida(url):
             self.url = url
         else:
             raise LookupError("Url Inválida!")
     
-    def url_eh_valida(self,url):
+    @staticmethod
+    def url_eh_valida(url):
         if url:
             return True
         else:
             return False
     
+    def retorna_moedas(self):
+        busca_moeda_origem = "moedaorigem"
+        busca_moeda_destino = "moedadestino"
+
+        inicio_substring_moeda_origem = self.encontra_indice_inicio_substring(busca_moeda_origem)
+        final_substring_moeda_origem = self.url.find("&")
+        moeda_origem = self.url[inicio_substring_moeda_origem:final_substring_moeda_origem]
+
+        inicio_substring_moeda_destino = self.encontra_indice_inicio_substring(busca_moeda_destino)
+        final_substring_moeda_destino = self.url.find("&valor")
+        moeda_destino = self.url[inicio_substring_moeda_destino:final_substring_moeda_destino]
+
+        return moeda_origem, moeda_destino
+
+
+    def encontra_indice_inicio_substring(self,moeda_ou_valor):
+        return self.url.find(moeda_ou_valor) + len(moeda_ou_valor) + 1
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from extrator_argumento_url import ExtratorArgumentoUrl
-url = 'https://www.google.com/search?q=pyhton&oq=pyhton&aqs=chrome..69i57j0l5.2644j0j7&sourceid=chrome&ie=UTF-8'
+url = "moedaorigem=real&moedadestino=dolar&valor=1500"
 argumento = ExtratorArgumentoUrl(url)
-print(argumento)
+print(argumento.retorna_moedas())
